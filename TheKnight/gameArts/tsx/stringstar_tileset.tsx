2d377ee7cd16cc53a411e2b1df8bb650dfcc7b06
Geoff Li <geoff.yulong.li@gmail.com>
2022-05-14 18:04:41 +0800

    Make a new version of executable file.

--- TheKnight/gameArts/tsx/stringstar_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="stringstar_tileset" tilewidth="16" tileheight="16" tilecount="198" columns="18">
- <image source="../tilesets/stringstar_tileset.png" width="288" height="176"/>
-</tileset>
